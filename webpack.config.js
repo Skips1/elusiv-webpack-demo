0fce4eae20ff27529097a3af014c257a66634158
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-06-22 16:53:36 +0530

    add loaders for css

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,11 @@ module.exports = {
       { 
         test: /\.(ts|tsx)$/, 
         loader: "ts-loader" 
-      }
+      },
+	  {
+		test: /\.css$/,
+		use: ['style-loader', 'css-loader'],
+	  },
     ],
   },
   plugins: [
